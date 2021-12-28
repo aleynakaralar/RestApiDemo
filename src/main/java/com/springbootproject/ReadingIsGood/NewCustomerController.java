bc32914389f71435024b6bb11008207f78838c89
Aleyna Karalar <karalaraleyna7@gmail.com>
2021-12-28 15:02:56 +0300

    adding delete method

--- src/main/java/com/springbootproject/ReadingIsGood/NewCustomerController.java
+++ src/main/java/com/springbootproject/ReadingIsGood/NewCustomerController.java
@@ -1,19 +1,24 @@
 package com.springbootproject.ReadingIsGood;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
 @RequestMapping("/newCustomer")
-public class NewCustomerController {
+public class NewCustomerController{
+
+
     private List<NewCustomerDto> newCustomerDtoList = new ArrayList<>();
 
     @PostMapping
-    public ResponseEntity<List<NewCustomerDto>> save(@RequestBody NewCustomerDto  newCustomerDto){
+    public ResponseEntity<List<NewCustomerDto>> save(@RequestBody NewCustomerDto newCustomerDto){
         newCustomerDtoList.add(newCustomerDto);
         return ResponseEntity.ok(newCustomerDtoList);
     }
@@ -23,4 +28,24 @@ public class NewCustomerController {
         return ResponseEntity.ok(newCustomerDtoList);
     }
     //TODO delete user by id
-}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<NewCustomerDto>> delete(@PathVariable int id){
+      //  System.out.println(newCustomerDtoList.size());
+        Iterator<NewCustomerDto> iter = newCustomerDtoList.iterator();
+        while(iter.hasNext()){
+            NewCustomerDto customer = iter.next();
+            if(customer.getId() == id){
+                iter.remove();
+            }
+
+        }
+
+        return ResponseEntity.ok(newCustomerDtoList);
+
+    }
+  }
+
+
+
+
